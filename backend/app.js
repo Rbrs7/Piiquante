83cffc0d18a05a9d7bb5fe4b7269ba7cb1359fb7
Rbrs7 <raphael.bressi@wanadoo.fr>
2023-01-25 14:34:49 +0100

    installation mongoose, app.js -> add mongo + message dans la console si connexion ou non

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://Rbrs7:raphael07@cluster0.wwwosg8.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB"))
+  .catch(() => console.log("Connexion à MongoDB échouée"));
+
+
+app.use((req, res) => {
+  res.json({ message: "Votre requête à bien été reçue" });
+});
 
 module.exports = app;
