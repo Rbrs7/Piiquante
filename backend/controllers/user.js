2b78444dac931e20ee717f817e8d8a3668613b6b
Rbrs7 <raphael.bressi@wanadoo.fr>
2023-02-09 15:43:20 +0100

    fin soutenance

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const secretToken = process.env.RANDOM_TOKEN_SECRET;
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
@@ -37,7 +38,7 @@ exports.login = (req, res, next) => {
             } else {
               res.status(200).json({
                 userId: user._id,
-                token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+                token: jwt.sign({ userId: user._id }, secretToken, {
                   expiresIn: "24h",
                 }),
               });
