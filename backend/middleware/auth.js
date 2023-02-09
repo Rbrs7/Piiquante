5adb9ca802b38abbb742cc0af62b9e3761020877
Rbrs7 <raphael.bressi@wanadoo.fr>
2023-02-09 15:46:57 +0100

    soutenance terminé

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,10 @@
 const jwt = require("jsonwebtoken");
+const secretToken = process.env.RANDOM_TOKEN_SECRET;
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(token, secretToken);
     const userId = decodedToken.userId;
     req.auth = {
       userId: userId,
